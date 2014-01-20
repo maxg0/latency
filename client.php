736aace52993fbc6ce41cf1e6564dae2a9ccbae5
Max Gräsbeck <grasbecm@arcada.fi>
2014-01-21 00:34:28 +0200

    latency and files

--- client.php
+++ client.php
@@ -1,12 +1,23 @@
 <?php
-for($i = 0; $i < 10; $i++){
+$averages = array();
+$firstTime = microtime(true);
+for($i = 0; $i < 10000; $i++){
+$startTime = microtime(true);
 $fp = stream_socket_client("udp://".$argv[1].":1113", $errno, $errstr);
 if (!$fp) {
-    echo "ERROR: $errno - $errstr<br />\n";
+	echo "ERROR: $errno - $errstr<br />\n";
 } else {
-    fwrite($fp, "\n");
-    echo fread($fp, 26);
-    fclose($fp);
+	fwrite($fp, "\n");
+	echo fread($fp, 26);
+	fclose($fp);
+	$endTime = microtime(true);
+	$total = $endTime - $startTime;
+	echo "$total ";
+	array_push($averages, $total);
 }
 }
-
+$lastTime = microtime(true);
+$totalTime = $lastTime - $firstTime;
+$average = array_sum($averages)/count($averages);
+echo "\naverage is: $average\n";
+echo "firstTime: $firstTime, lastTime: $lastTime\n totaling: $totalTime";
